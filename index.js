b2b14b94c390f0ba462f95213b3b2119f0cc1380
jordnkr <jordnkr408@gmail.com>
2022-04-02 00:52:43 -0500

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const chromeLauncher = require('chrome-launcher');
 try {
   // `url` input defined in action metadata file
   const testUrl = core.getInput('url');
-
+  console.log(`------TESTING URL: ${testUrl}`)
   const chrome = chromeLauncher.launch({chromeFlags: ['--headless']});
   const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
   const runnerResult = lighthouse(testUrl, options);
@@ -16,7 +16,7 @@ try {
   //console.log("-------------RUN RESULT------------");
   //console.log(JSON.stringify(runnerResult.lhr));
   //console.log('Report is done for', runnerResult.lhr.finalUrl);
-  //console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  console.log('CONSOLE LOG Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
   chrome.kill();
 } catch (error) {
