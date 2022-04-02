5af99549afbb0aa7874937c6fec27fbaa65fbf12
jordnkr <jordnkr408@gmail.com>
2022-04-02 01:10:13 -0500

    remove output file

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const chromeLauncher = require('chrome-launcher');
   const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
   const runnerResult = await lighthouse(testUrl, options);
 
-  // `.report` is the HTML report as a string
-  const reportHtml = runnerResult.report;
-  fs.writeFileSync('lhreport.html', reportHtml);
-
   // `.lhr` is the Lighthouse Result as a JS object
   core.setOutput("performancescore", runnerResult.lhr.categories.performance.score * 100);
   console.log('Report is done for', runnerResult.lhr.finalUrl);
