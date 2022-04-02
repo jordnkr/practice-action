46a443094e2e0a9ba10c0990a127fd5d4078dd98
jordnkr <jordnkr408@gmail.com>
2022-04-01 22:38:33 -0500

    creating action

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+
+try {
+  // `url` input defined in action metadata file
+  const testUrl = core.getInput('url');
+
+  const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  const options = {logLevel: 'info', output: 'html', onlyCategories: ['performance'], port: chrome.port};
+  const runnerResult = await lighthouse(testUrl, options);
+
+  // `.lhr` is the Lighthouse Result as a JS object
+  core.setOutput("performancescore", runnerResult.lhr.score.categories.performance.score * 100);
+  //console.log("-------------RUN RESULT------------");
+  //console.log(JSON.stringify(runnerResult.lhr));
+  //console.log('Report is done for', runnerResult.lhr.finalUrl);
+  //console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+  await chrome.kill();
+} catch (error) {
+  core.setFailed(error.message);
+}
